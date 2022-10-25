92e842a27480632a33bb065cbd833d3bd0144101
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2022-10-25 17:10:42 -0300

    configurado protocolos e delegate na homeVC

--- messengerApp/messengerApp/Home/Controller/HomeViewController.swift
+++ messengerApp/messengerApp/Home/Controller/HomeViewController.swift
@@ -6,10 +6,21 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewController: UIViewController {
 	
+	var auth: Auth?
+	var db: Firestore?
+	var idUsuarioLogado: String?
+	
+	var screenContact:Bool?
+	var emailUsuarioLogado: String?
+	var alert: Alert?
+	
 	var homeView: HomeView?
+	
 	
 	override func loadView() {
 		homeView = HomeView()
@@ -20,7 +31,55 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 		 navigationController?.navigationBar.isHidden = true
 		 view.backgroundColor = CustomColor.appLight
+		 configHomeView()
+		 configCollectionView()
+		 configAlert()
     }
 	
+	private func configHomeView(){
+		homeView?.navView.delegate(delegate: self)
+	}
+	
+	private func configCollectionView(){
+		self.homeView?.delegateCollectionView(delegate: self, dataSource: self)
+	}
+	
+	private func configAlert(){
+		alert = Alert(controller: self)
+	}
+	
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		return UICollectionViewCell()
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		print(indexPath)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: collectionView.frame.width, height: 100)
+	}
+}
+
+
+extension HomeViewController: NavViewProtocol{
+	
+	func typeScreenMessage(type: TypeChatOrContact) {
+		switch type {
+		case .contact:
+			self.screenContact = true
+		case .chat:
+			self.screenContact = false
+		}
+	}
 
 }
+
